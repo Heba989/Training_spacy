39e66d7f87fa4d42c53598e7fbd4bd7b20c78446
Heba989 <9892044539@htu.edu.jo>
2023-06-21 10:26:31 +0300

    change config path

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ from datetime import date
 from spacy.cli.train import train
 from spacy import displacy
 import spacy_transformers
-
+# import stop_vm
 
 # # define train data path
-configpath = 'config_beart.cfg'
+configpath = 'config_beart_lr2.cfg'
 print('GCS')
 day = date.today()
 # define model path
@@ -19,3 +19,5 @@ train_path = 'data/train2023-06-14.spacy'
 valid_path = 'data/dev2023-06-14.spacy'
 print('train must be starting now')
 train(config_path=configpath, output_path=model,overrides={"paths.train": train_path, "paths.dev": valid_path}, use_gpu=-1)
+# this to stop vm after the training finish
+# stop_vm.stop_instance('kayanhr-staging', 'europe-west1-b', 'kayanhr-staging-ai-workload-1')
